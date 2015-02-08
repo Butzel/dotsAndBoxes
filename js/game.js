aac3191fda28cbaa893d0b4c400fa56448fb386c
Butzel <mibu@maite.de>
2015-02-08 20:00:37 +0100

    outsourced function drawTable

--- js/game.js
+++ js/game.js
@@ -1,25 +1,25 @@
-function drawTable() {
+function drawTable(fieldSize) {
 	document.write('<table><tbody>');
-	for (var i = 0; i < 6; i++) {
+	for (var i = 0; i < fieldSize; i++) {
 		document.write('<tr>');
-		for (var a = 0; a < 6; a++) {
+		for (var a = 0; a < fieldSize; a++) {
 			document.write('<td id="middle_cell_' + i + '_' + a
 					+ '" class="middle_cell">middle_cell_' + i + '_' + a
 					+ '</td>');
-			if (a != 5) {
+			if (a != fieldSize -1) {
 				document.write('<td id="upper_cell_wrap_' + i + '_' + a
 						+ '" class="upper_cell_wrap">upper_cell_wrap_' + i
 						+ '_' + a + '</td>');
 			}
 		}
 		document.write('</tr>');
-		if (i != 5) {
+		if (i != fieldSize -1) {
 			document.write('<tr>');
-			for (var b = 0; b < 6; b++) {
+			for (var b = 0; b < fieldSize; b++) {
 				document.write('<td id="left_cell_wrap_' + i + '_' + b
 						+ '" class="left_cell_wrap">left_cell_wrap_' + i + '_'
 						+ b + '</td>');
-				if (b != 5) {
+				if (b != fieldSize -1) {
 					document.write('<td id="main_cell_' + i + '_' + b
 							+ '" class="main_cell">main_cell_' + i + '_' + b
 							+ '</td>');
