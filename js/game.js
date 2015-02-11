7ecf56f7af4f37ed8efea7e905e26f74c462fb54
Butzel <mibu@maite.de>
2015-02-11 20:53:49 +0100

    changed some style class names and formatting of source code

--- js/game.js
+++ js/game.js
@@ -4,7 +4,8 @@
 var usedCellsPlayerOne = 0;
 // Anzahl der erzielten Punkte auf den Startwert 0 setzen
 var gainedPointsPlayerOne = 0;
-
+// Initial den aktuellen Spieler auf playerOne setzen.
+var actualPlayer = 'playerOne';
 
 // Funktion zur dynamischen Erzeugung des Spielfelds
 function drawField(fieldSize) {
@@ -20,40 +21,36 @@ function drawField(fieldSize) {
 	document.write('<td id=gainedPointsPlayerOne></td>');
 	document.write('</tr>');
 	document.write('</tbody></table></p>');
-	
-	// Erstmaliges Ändern der verwendeten Spielersteine bei usedCellsPlayerOne = 0
+
+	// Erstmaliges Ändern der verwendeten Spielersteine bei usedCellsPlayerOne=0
 	document.getElementById('usedCellsPlayerOne').innerText = usedCellsPlayerOne;
-	
+
 	// Erstmaliges Ändern der erzielten Punkte bei gainedPointsPlayerOne = 0
 	document.getElementById('gainedPointsPlayerOne').innerText = gainedPointsPlayerOne;
-	
+
 	// fieldSize von vorne herein um 1 erhöhen, damit die letzte untere
 	// und die letzte linke Reihe mit Zellen befüllt werden kann.
 	fieldSize++;
-	
+
 	// Spielfeld zeichnen und Ids der Zellen festlegen
 	document.write('<table class="gameField"><tbody>');
 	for (var i = 0; i < fieldSize; i++) {
 		document.write('<tr>');
 		for (var a = 0; a < fieldSize; a++) {
-			document.write('<td id="middle_cell_' + i + '_' + a
-					+ '" class="middle_cell"></td>');
+			document.write('<td id="centerCell_' + i + '_' + a	+ '" class="centerCell"></td>');
 			if (a != fieldSize - 1) {
-				upperCellId = 'upper_cell_wrap_' + i + '_' + a;
-				document.write('<td id="upper_cell_wrap_' + i + '_' + a
-						+ '" class="upper_cell_wrap" onClick="setMark(\'' + upperCellId + '\')"></td>');
+				upperCellId = 'upperCellWrap_' + i + '_' + a;
+				document.write('<td id="upperCellWrap_'	+ i	+ '_' + a + '" class="upperCellWrap" onClick="setMarkPlayerOne(\'' + upperCellId + '\')"></td>');
 			}
 		}
 		document.write('</tr>');
 		if (i != fieldSize - 1) {
 			document.write('<tr>');
 			for (var b = 0; b < fieldSize; b++) {
-				leftCellId = 'left_cell_wrap_' + i + '_' + b;
-				document.write('<td id="left_cell_wrap_' + i + '_' + b
-						+ '" class="left_cell_wrap" onClick="setMark(\'' + leftCellId + '\')"></td>');
+				leftCellId = 'leftCellWrap_' + i + '_' + b;
+				document.write('<td id="leftCellWrap_' + i + '_' + b + '" class="leftCellWrap" onClick="setMarkPlayerOne(\'' + leftCellId + '\')"></td>');
 				if (b != fieldSize - 1) {
-					document.write('<td id="main_cell_' + i + '_' + b
-							+ '" class="main_cell""></td>');
+					document.write('<td id="mainCell_' + i + '_' + b + '" class="mainCell"">mainCell_' + i + '_' + b + '</td>');
 				}
 			}
 			document.write('</tr>');
@@ -65,32 +62,33 @@ function drawField(fieldSize) {
 
 // Funktion zum setzen der Farben der Zellen
 
-function setMark(cellId) {
+function setMarkPlayerOne(cellId) {
 
 	// Auslesen der aktuellen Stylesheet-Klasse
 	var cellClass = document.getElementById(cellId).className;
-	
+
 	// Es wird geprüft welche Klasse zugewiesen ist.
-	// Bei einem unbenutzten Balken wird die Klasse gesetzt, 
+	// Bei einem unbenutzten Balken wird die Klasse gesetzt,
 	// die ihn als einen vom Spieler benutzten Balken ausweist.
 	// Außerdem wird die Anzahl der benutzen Balken um eins erhöht.
-	switch(cellClass) {
-		case 'left_cell_wrap':
-			document.getElementById(cellId).className = 'left_cell_wrap_used_player_one';
-			usedCellsPlayerOne++;
-			checkPoints();
-			break;
-		case 'upper_cell_wrap':
-			document.getElementById(cellId).className = 'upper_cell_wrap_used_player_one';
-			usedCellsPlayerOne++;
-			checkPoints();
-			break;
+	switch (cellClass) {
+	case 'leftCellWrap':
+		document.getElementById(cellId).className = 'leftCellWrapUsedPlayerOne';
+		gainedPoints(cellId);
+		break;
+	case 'upperCellWrap':
+		document.getElementById(cellId).className = 'upperCellWrapUsedPlayerOne';
+		gainedPoints(cellId);
+		break;
 	}
-	
-	// Regelmäßige Ausgabe der vom Spieler verwendeten Steine
-	document.getElementById('usedCellsPlayerOne').innerText = usedCellsPlayerOne;
 }
 
-// Wurden durch das Setzen des Steins punkte erzielt?
-function gainedPoints() {
+// Wurden durch das Setzen des Steins Punkte erzielt?
+function gainedPoints(cellId) {
+
+	// Die Anzahl der verwendeten Stein um Eins erhöhen
+	usedCellsPlayerOne++;
+
+	// Ausgabe der vom Spieler verwendeten Steine
+	document.getElementById('usedCellsPlayerOne').innerText = usedCellsPlayerOne;
 }
